920439b163937e219dc3eb1759ffd211ea1d6ae6
cpinedop <carlos.pinedop@gmail.com>
2021-02-01 16:43:40 +0100

    using cloud config

--- microservice01/src/main/java/com/crkcr/microservice01/Microservice01Application.java
+++ microservice01/src/main/java/com/crkcr/microservice01/Microservice01Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Microservice01Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Microservice01Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Microservice01Application.class, args);
+    }
 
 }
